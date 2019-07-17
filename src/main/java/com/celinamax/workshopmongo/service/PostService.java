a511aca03f6ba3c2d0817800bb6443a713609942
Celina Maximiano <git config --global user.name Celina>
2019-07-17 19:21:29 -0300

    Consulta simples com query methods

--- src/main/java/com/celinamax/workshopmongo/service/PostService.java
+++ src/main/java/com/celinamax/workshopmongo/service/PostService.java
@@ -1,5 +1,7 @@
 package com.celinamax.workshopmongo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +22,9 @@ public class PostService {
 		 }		 
 		 return post;
 	}	
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 	
 }
