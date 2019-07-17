a4c5f837fd1a21fd7373725aa9d51a358e7e6a56
Celina Maximiano <git config --global user.name Celina>
2019-07-17 17:51:36 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/celinamax/workshopmongo/config/Instantiation.java
+++ src/main/java/com/celinamax/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.celinamax.workshopmongo.domain.Post;
 import com.celinamax.workshopmongo.domain.User;
 import com.celinamax.workshopmongo.dto.AuthorDTO;
+import com.celinamax.workshopmongo.dto.CommentDTO;
 import com.celinamax.workshopmongo.repository.PostRepository;
 import com.celinamax.workshopmongo.repository.UserRepository;
 
@@ -21,7 +22,7 @@ public class Instantiation implements CommandLineRunner{
 	private UserRepository userRepository;
 	
 	@Autowired
-	private PostRepository postRepository;
+	private PostRepository postRepository;	
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -41,11 +42,20 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(noah));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(noah));
 		
-				
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(cecilia));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(celina));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(celina));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
+		
 		postRepository.save(Arrays.asList(post1,post2));
 		
 		noah.getPosts().addAll(Arrays.asList(post1,post2));
 		userRepository.save(noah);
+		
+	
 		
 		
 		
